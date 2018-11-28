79092a467a4ecc8d572b8ee35c2e6fe4c4e28f60
Langleu <langleu90@gmail.com>
2018-11-28 19:53:58 +0100

    add remaning csv

--- assignment 1/solution/measure-disk-random.sh
+++ assignment 1/solution/measure-disk-random.sh
@@ -2,6 +2,10 @@
 
 # creates a 1GB testfile with 1M read/write at a time. The ratio is 75/25 meaning 3:1 for read and write.
 # 3 reads are followed by 1 write and 64 Operations at a time.
+# for vio v3
+READWRITE="$(fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=random --filename=randomFile --bs=1M --iodepth=64 --size=1024M --readwrite=randrw -rwmixread=75 2>&1 | awk '/READ: bw=/ {printf $2 }')"
+
+# for vio v2
 READWRITE="$(fio --randrepeat=1 --ioengine=libaio --direct=1 --gtod_reduce=1 --name=random --filename=randomFile --bs=1M --iodepth=64 --size=1024M --readwrite=randrw -rwmixread=75 2>&1 | awk '/aggrb/ {printf substr($3,7,length($3)-6) }')"
 
 # removes tempfile just in case
